e6e7b9508db44b062b430480d3dd77f3771f4785
youchanpark <pakyhc@gmail.com>
2019-12-30 21:04:39 +0900

    img 21 added

--- app/src/main/java/com/example/practice/ui/main/SecondFragment.java
+++ app/src/main/java/com/example/practice/ui/main/SecondFragment.java
@@ -44,7 +44,7 @@ public class SecondFragment extends Fragment {
 
     public class MyGridAdapter extends BaseAdapter {
         Context context;
-        Integer[] imgID = {R.drawable.img01, R.drawable.img02, R.drawable.img03, R.drawable.img04,R.drawable.img05, R.drawable.img06, R.drawable.img07, R.drawable.img08, R.drawable.img09, R.drawable.img10, R.drawable.img11, R.drawable.img12, R.drawable.img13, R.drawable.img14, R.drawable.img15, R.drawable.img16, R.drawable.img17, R.drawable.img18, R.drawable.img19, R.drawable.img20,};
+        Integer[] imgID = {R.drawable.img01, R.drawable.img02, R.drawable.img03, R.drawable.img04,R.drawable.img05, R.drawable.img06, R.drawable.img07, R.drawable.img08, R.drawable.img09, R.drawable.img10, R.drawable.img11, R.drawable.img12, R.drawable.img13, R.drawable.img14, R.drawable.img15, R.drawable.img16, R.drawable.img17, R.drawable.img18, R.drawable.img19, R.drawable.img20, R.drawable.img21};
 
         public MyGridAdapter(Context C) { context = C;}
 
